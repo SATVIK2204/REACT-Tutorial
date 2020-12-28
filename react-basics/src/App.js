d7a5522def5d86bebc4bcd7e887dd5a18da53ff6
SATVIK2204 <bksatvik@gmail.com>
2020-12-28 18:03:54 +0530

    Added 5_state.js

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,28 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-// import Greet from './components/greeting'
-// import Welcome from './components/welcome.js'
-// import Hello from './components/3_with_jsx.js'
-// import Hello from './components/3_without_jsx'
-// import GreetCustom from './components/4_props'
-import Welcomeprop from './components/4_props_class_component'
+
+import Message from './components/5_state'
 function App() {
   return (
     <div className="App">
-      {/* <GreetCustom name="Mr. Stark" heroname="Iron Man">
-        <p>Genius | Billionare| Playboy | Philanthrophist</p>
-      </GreetCustom>
-      <GreetCustom name="Steve Rogers" heroname="Captain America">
-        <button>I can do this all day?</button>
-      </GreetCustom>
-      <GreetCustom name="Thor Odinson" heroname="Thod-God of Thunder"/> */}
-      <Welcomeprop name="Mr. Stark" heroname="Iron Man">
-        <p>Genius | Billionare| Playboy | Philanthrophist</p>
-      </Welcomeprop>
-      <Welcomeprop name="Steve Rogers" heroname="Captain America">
-        <button>I can do this all day?</button>
-      </Welcomeprop>
-      <Welcomeprop name="Thor Odinson" heroname="Thod-God of Thunder"/>
+     <Message/>
     </div>
   );
 }
