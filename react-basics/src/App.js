b325108ee47e20f626fbd27ffd9cc90aaa3b4fc2
SATVIK2204 <bksatvik@gmail.com>
2020-12-28 20:13:16 +0530

    Added event handling

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-// import GreetCustom from './components/7_destructuring_props_state_functional'
-import Welcomeprop from './components/7_destructuring_props_state_functional'
+// import FunctionClick from './components/8_event_handling_functional'
+import ClassClick from './components/8_event_handling_class'
 function App() {
   return (
     <div className="App">
-    <Welcomeprop name="Mr. Stark" heroname="Iron Man"/>
+    <ClassClick/>
     </div>
   );
 }
